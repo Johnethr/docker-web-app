a1a4f102bf1fa2e8a39e77856a664a5b55da569c
Josh-Tse <97326574+Josh-Tse@users.noreply.github.com>
2024-03-08 01:33:01 +0000

    commit 2 dockerfile approach

--- version_2/docker-compose approach/login_page.php
+++ version_2/docker-compose approach/login_page.php
@@ -7,8 +7,10 @@ $pword = $_POST["psw"];
 
 $pwordhsh=password_hash("#Itzthebozz20",PASSWORD_DEFAULT);
 
+// comparing hash with password
 $vpword=password_verify($_POST["psw"],$pwordhsh);
 
+// response
 if ($uname == "ADMIN" && $psw == 1) {
     echo "Login successful!";
     echo "Welcome Admin";
